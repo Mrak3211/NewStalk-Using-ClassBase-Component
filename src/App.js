f6fe33b2b69f738989ab76615ad13ebfd3ae0138
Ayush <ayushchodvadiya9898@gmail.com>
2023-06-13 15:37:48 +0530

    NewsTalk Using ClassBase Components

---
+++ src/App.js
@@ -0,0 +1,138 @@
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar";
+import News from "./Components/News";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
+
+export default class App extends Component {
+  apiKey = process.env.REACT_APP_NEWSTALK_API;
+
+  state = {
+    progress: 0,
+  };
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <LoadingBar
+            height={3}
+            color="#f11946"
+            progress={this.state.progress}
+          />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="general"
+                  apiKey={this.apiKey}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/business"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="business"
+                  apiKey={this.apiKey}
+                  country="in"
+                  category="business"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="entertainment"
+                  apiKey={this.apiKey}
+                  country="in"
+                  category="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/general"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="general"
+                  apiKey={this.apiKey}
+                  country="in"
+                  category="general"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/health"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="health"
+                  apiKey={this.apiKey}
+                  country="in"
+                  category="health"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/science"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="science"
+                  apiKey={this.apiKey}
+                  country="in"
+                  category="science"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="sports"
+                  apiKey={this.apiKey}
+                  country="in"
+                  category="sports"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  key="technology"
+                  apiKey={this.apiKey}
+                  country="in"
+                  category="technology"
+                />
+              }
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
